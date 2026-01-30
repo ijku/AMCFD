33efd3c23ddd475d3d1439b538eeb6a09cb3a189
root <root@EN4226766.fulton.ad.asu.edu>
2026-01-30 09:48:15 -0700

    Replace repository contents with local files

---
+++ clean.sh
@@ -0,0 +1,8 @@
+find -name "*.o" -exec rm -f '{}' \;
+find -name "*.f90~" -exec rm -f '{}' \;
+find -name "*.mod" -exec rm -f '{}' \;
+find -name "*.log" -exec rm -f '{}' \;
+find -name "cluster_main.*" -exec rm -f '{}' \;
+
+cd result
+find -name "*.tec" -exec rm -f '{}' \;
